9ec9fd908886e9f293facd311776a89555a7fb29
Wai Yan Myint Mo <dragonkey100@gmail.com>
2019-11-18 23:43:36 +0630

    added more info to yaml

--- App/themer.py
+++ App/themer.py
@@ -1,6 +1,6 @@
 # Replace function
-def theme(template_local: str, keyword_list_local: list, keyword_config_local: dict) -> str:
-    for keyword in keyword_list_local:
+def theme(template_local: str, keyword_config_local: dict) -> str:
+    for keyword in keyword_config_local.keys():
         config = keyword_config_local[keyword]
         template_local = template_local.replace(keyword, config)
     return template_local
@@ -13,14 +13,12 @@ def theme(template_local: str, keyword_list_local: list, keyword_config_local: d
 
 
 if __name__ == "__main__":
-    # Keywords are SvrName and Dir1
-    keyword_list = ['SvrName', 'Dir1']
     # Replace SvrName with Example1.com and Dir1 with D:/Example1
     keyword_config = {'SvrName': 'Example1.com',
                       'Dir1': r'D:\Example1'}
     # We got the template string and it is "i love my SvrName located in Dir1 Directory"
     template = "i love my SvrName located in Dir1 Directory"
-    output = theme(template, keyword_list, keyword_config)
+    output = theme(template, keyword_config)
 
     # Test Case
     expected = r"i love my Example1.com located in D:\Example1 Directory"
