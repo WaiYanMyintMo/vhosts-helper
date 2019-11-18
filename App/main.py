2ce510438cbeee3c63111f30f9c11193f44f3d8c
Wai Yan Myint Mo <dragonkey100@gmail.com>
2019-11-18 19:37:24 +0630

    added constants to start of file

---
+++ App/main.py
@@ -0,0 +1,11 @@
+# //Must Have Outline
+# Host and Vhost assigned at top of file.
+# Server Name and DocumentRoot assigned at top of file.
+# Function for opening file, reading and returning replaced data.
+# Function for appending data to a file.
+
+HostsPath = 'C:\\Windows\\System32\\drivers\\etc\\hosts'
+VhostsPath = 'C:\\xampp\\apache\\conf\\extra\\httpd-vhosts.conf'
+ServerName = 'testing1.com'
+DocumentRoot = 'D:\\Testing\\'
+
